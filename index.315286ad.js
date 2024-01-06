079cbe87cfcebd2faa66952af756c7904a28bc8c
Dmytro Fefelov <reseneweb@gmail.com>
2024-01-06 17:59:24 +0200

    make build

---
+++ index.315286ad.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=parseFloat(getComputedStyle(e).width),o=parseFloat(getComputedStyle(e).height),a=parseFloat(getComputedStyle(t).width),p=parseFloat(getComputedStyle(t).height);t.style.top="".concat((l-a)/2,"px"),t.style.left="".concat((o-p)/2,"px");
+//# sourceMappingURL=index.315286ad.js.map
